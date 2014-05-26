54c88951b357d18f1b35dc288f1930fbd2a0a8c5
Oted <herr.olle@gmail.com>
2014-05-27 00:59:35 +0200

    add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		compass: {
+            dist: {
+                options: {
+                    sassDir: "public/stylesheets/sass/",
+                    cssDir: "public/stylesheets/css/",
+                    httpPath : "/"
+                }
+            }
+        },
+		browserify : {
+			default : {
+				files : {
+					"public/script.js" : 	["public/javascripts/index.js", "public/javascripts/user.js", "public/javascript/ajax.js"],
+                }	
+			}
+		},
+		watch : {
+			script : {
+				files : ["public/javascripts/*.js"],
+				tasks : ["browserify"]
+			},
+			options: {
+         	   livereload: 1338,
+			   atBegin : true
+        	},
+        	css: {
+          		files: ["public/stylesheets/sass/*.scss"],
+          		tasks: ["compass"]
+        	}
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-browserify");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.registerTask("default",["browserify","compass"]);
+	grunt.loadNpmTasks('grunt-contrib-compass');
+};
