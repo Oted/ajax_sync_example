c2339d0e73163b0a6e47d5282a4b564ff1a39aa1
Oted <herr.olle@gmail.com>
2014-05-27 15:05:55 +0200

    add id to response object

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,10 @@ module.exports.index = function(req, res){
 //request users.
 //obs! dummy data here, no actual users exist 
 module.exports.users = function(req, res){
+    var id = req.body.id; 
     if (Math.random() < 0.5){
-        res.jsonp({"online": true});
+        res.jsonp({"id": id, "online": true});
     } else {
-        res.jsonp({"online": false});
+        res.jsonp({"id": id, "online": false});
     }
 };
