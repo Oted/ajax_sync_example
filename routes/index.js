30986b76c5c06b67c40cc7f3311ed7878cad1e1d
Oted <herr.olle@gmail.com>
2014-05-27 04:28:08 +0200

    minor UI changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ module.exports.index = function(req, res){
     res.render("index");
 };
 
-//request users
+//request users.
+//obs! dummy data here, no actual users exist, 
+//as i understood it the focus is on the front-end.
 module.exports.users = function(req, res){
-
+    if (Math.random() < 0.5){
+        res.jsonp({"id": req.params.id, "online": true});
+    } else {
+        res.jsonp({"id": req.params.id, "online": false});
+    }
 };
